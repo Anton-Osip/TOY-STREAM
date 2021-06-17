03abb087ed1d2ad0cbd0b7331910cc4b2fa01e0e
Anton Osipchik <osipchika@gmail.com>
2021-06-17 13:54:38 +0300

    создал проект

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import './sass/main.scss'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+	<div className='container'>
+		<h1>Toy stream</h1>
+	</div>
+)
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+const path = require('path')
+const webpack = require('webpack')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+	context: path.resolve(__dirname, 'src'),
+	mode: 'development',
+	entry: {
+		main: ['@babel/polyfill', './index.jsx'],
+	},
+	output: {
+		filename: '[name].[contenthash].js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	devServer: {
+		historyApiFallback: true,
+		contentBase: path.resolve(__dirname, './dist'),
+		open: true,
+		compress: true,
+		hot: true,
+		port: 8080,
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			title: 'Toy stream',
+			template: path.resolve(__dirname, './src/index.html'),
+		}),
+		new CleanWebpackPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		// new CopyWebpackPlugin({
+		// 	patterns: [
+		// 		{
+		// 			from: path.resolve(
+		// 				__dirname,
+		// 				'./src/assets/img/contacts-bg.png'
+		// 			),
+		// 			to: path.resolve(__dirname, './dist'),
+		// 		},
+		// 	],
+		// }),
+		new MiniCssExtractPlugin({
+			filename: '[name].[contenthash].css',
+		}),
+	],
+	module: {
+		rules: [
+			//CSS
+			// {
+			// 	test: /\.css$/,
+			// 	use: [
+			// 		{
+			// 			loader: MiniCssExtractPlugin.loader,
+			// 		},
+			// 		'css-loader',
+			// 	],
+			// },
+			//scss
+			{
+				test: /\.s[ac]ss$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					'css-loader',
+					'sass-loader',
+				],
+			},
+			// изображения
+			{
+				test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+				type: 'asset/resource',
+			},
+			// шрифты и SVG
+			{
+				test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+				type: 'asset/inline',
+			},
+			// JavaScript
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
+			},
+			//REACT
+			{
+				test: /\.jsx?$/, // определяем тип файлов
+				exclude: /(node_modules)/, // исключаем из обработки папку node_modules
+				loader: 'babel-loader', // определяем загрузчик
+				options: {
+					presets: ['@babel/preset-env', '@babel/preset-react'], // используемые плагины
+				},
+			},
+		],
+	},
+}
